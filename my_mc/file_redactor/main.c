e923b029897355bbbd96b114898f2240cfa77a8d
santonet <santonet941839@gmail.com>
2023-07-10 21:57:16 +0700

    need build in main

--- my_mc/file_redactor/main.c
+++ my_mc/file_redactor/main.c
@@ -17,6 +17,7 @@
 #include <assert.h>
 
 #define CNT_BYTE_READ 20
+
 extern WINDOW *stdscr;
 
 void sig_winch(int signo)
@@ -61,8 +62,9 @@ void draw_file(int file_descriptor) {
 
 }
 
-void save_file(FILE *fp) {
+void save_file(FILE *fp, int file_descriptor) {
 
+    lseek(file_descriptor, 0, SEEK_SET);
     int max_y = 0, max_x = 0;
     getmaxyx(stdscr, max_y, max_x);
     // fprintf(fp, "max_y: %d\nmax_x: %d\n",  max_y, max_x);
@@ -87,12 +89,18 @@ void save_file(FILE *fp) {
         if (counter_LF == 0) {
             // buf[index++] = '\n'; 
             fprintf(fp, "\n");
+            write(file_descriptor, "\n", 1);
+
         } else {
             buf[index] = '\0';
             reverse(buf);
+            buf[index++] = '\n';
+            buf[index] = '\0'; 
+
             if (strlen(buf) > 0) {
                 // fprintf(fp, "|");
-                fprintf(fp, "%s\n", buf);
+                fprintf(fp, "%s", buf);
+                write(file_descriptor, buf, strlen(buf));
 
             }
 
@@ -102,37 +110,48 @@ void save_file(FILE *fp) {
 
 }
 
-int main(int argc, char ** argv) {
-    FILE *fp = fopen("OUTPUT.txt", "w");
-    initscr();
+void print_menu(WINDOW *MENU_WIND, WINDOW *MENU_BORDER) {
+
+
+
+    box(MENU_BORDER, '|', '-');
+    wprintw(MENU_WIND, "F(1): save file, F(2): open/create file, F(3): exit\n");
+    wrefresh(MENU_BORDER);
+
     
-    signal(SIGWINCH, sig_winch);
-    cbreak(); 
-    noecho();
-    curs_set(TRUE);
-    // idlok(stdscr, TRUE);
-    // scrollok(stdscr, TRUE);
-    // scroll(stdscr);
-    
+
+}
+
+int print_file(char *filename) {
+
     attron(A_BOLD);
-    int file_descriptor = open("file2.txt", O_CREAT|O_RDWR, S_IRWXU);
-    
+    int file_descriptor = open(filename, O_CREAT|O_RDWR, S_IRWXU);
     draw_file(file_descriptor);
 
 
     attroff(A_BOLD);
     refresh();
 
-    keypad(stdscr, TRUE); // Для обработки esc спец символов
+    return file_descriptor;
+}
+
+
+int write_scr(FILE *fp, int file_descriptor, WINDOW *MENU_WIND, WINDOW *MENU_BORDER) {
+    // int status_new_file = 1, status_exit = 0;
+    int status = 0;
     
     int ch;
     int y = 0, x = 0;
+    int flag_break = 0;
     wmove(stdscr, y, x);
     while( (ch = wgetch(stdscr)) != KEY_F(3)) {
         getyx(stdscr, y, x);
+        
 
         switch (ch) {
         case KEY_BACKSPACE:
+            delch();
+            // обработать \n, а точнее удаление строки
             break;
 
         case KEY_LEFT:
@@ -152,17 +171,74 @@ int main(int argc, char ** argv) {
             break;
 
         case KEY_F(1):
-            save_file(fp);
+            save_file(fp, file_descriptor);
             break;
 
+        case KEY_F(2):
+            flag_break = 1;
+            status = 1;
+            break;
+
+        case KEY_F(3):
+            flag_break = 1;
+            break;
 
         default:
             wechochar(stdscr, ch);
             break;
         }
+
+        if (flag_break) break;
     }
+
     close(file_descriptor);
     fclose(fp);
+
+    return status;
+}
+
+void main_fucntions(char *filename, FILE *fp, WINDOW *MENU_WIND, WINDOW *MENU_BORDER) {
+
+    int file_descriptor = print_file(filename);
+    print_menu(MENU_WIND, MENU_BORDER);
+
+    write_scr(fp, file_descriptor, MENU_WIND, MENU_BORDER);
+}
+
+int main(int argc, char ** argv) {
+
+    WINDOW *MENU_WIND, *MENU_BORDER;
+    FILE *fp = fopen("OUTPUT.txt", "w"); // для отладки
+    initscr();
+    
+    signal(SIGWINCH, sig_winch);
+    cbreak();
+    noecho();
+    curs_set(TRUE);
+    keypad(stdscr, TRUE); // Для обработки esc спец символов
+    // idlok(stdscr, TRUE);
+    // scrollok(stdscr, TRUE);
+    // scroll(stdscr);
+
+    int max_y, max_x;
+    getmaxyx(stdscr, max_y, max_x);
+
+    MENU_BORDER = newwin(4, max_x, max_y - 4, 0);
+    MENU_WIND = derwin(MENU_BORDER, 2, max_x - 2, 1, 1);
+
+    while(1) {
+        // main_fucntions("file3.txt", fp, MENU_WIND, MENU_BORDER);
+        int file_descriptor = print_file("temp.out");
+        print_menu(MENU_WIND, MENU_BORDER);
+
+        int status = write_scr(fp, file_descriptor, MENU_WIND, MENU_BORDER);
+        
+        if (0 == status) break;
+    }
+
+
+    delwin(MENU_WIND);
+    delwin(MENU_BORDER);
 
     endwin();
     exit(EXIT_SUCCESS);
