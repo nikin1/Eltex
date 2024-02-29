4bac785e4424394d01ab215f2261b4290352a5c2
santonet <nik941839@gmail.com>
2024-02-29 14:13:34 +0700

    not wokr 0.2

--- SOCKETS_IPC/lecture_5_mac_header/client.c
+++ SOCKETS_IPC/lecture_5_mac_header/client.c
@@ -43,7 +43,7 @@ int check_sum(char *buf) {
 
     ptr = (short *) buf;
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 10; i++) {
         csum = csum + *ptr; // maybe + *ptr ??? 
         ptr++;
     }
@@ -53,6 +53,33 @@ int check_sum(char *buf) {
     csum = ~csum;
 
     return csum;
+
+    /* Compute Internet Checksum for "count" bytes
+     * beginning at location "addr".
+     */
+    // int count = 20;
+    // register long sum = 0, checksum;
+
+    // short *addr;
+    // addr = (short *) buf;
+
+    // while( count > 1 )  {
+    //     /* This is the inner loop */
+    //     sum += * (unsigned short *) addr++;
+    //     count -= 2;
+    // }
+
+    // /*  Add left-over byte, if any */
+    // if ( count > 0 )
+    //     sum += * (unsigned char *) addr;
+
+    // /*  Fold 32-bit sum to 16 bits */
+    // while (sum>>16)
+    //     sum = (sum & 0xffff) + (sum >> 16);
+
+    // checksum = ~sum;
+
+    // return checksum;
 }
 
 
@@ -60,13 +87,13 @@ int main() {
     int fd;
     struct sockaddr_ll serv_addr;
     socklen_t serv_size;
-    struct in_addr serv_ip_adress;
+    struct in_addr serv_ip_adress, client_ip_adress;
     char *string_data = NULL;
 
 
     struct udphdr *udp_hdr = NULL;
     char buffer[1024] = {0};
-    char msg[] = "hello";
+    char msg[] = "hello!";
 
     char recv_buf[128], *recv_string_data = NULL;
     
@@ -80,10 +107,16 @@ int main() {
     if (fd == -1) handle_error("socket");
 
 
-
-
+    printf("<<<<0>>>>\n");
     // SERV_STRUCT
-    if (inet_pton(AF_INET, "127.0.0.1", &(serv_ip_adress.s_addr) ) != 1) handle_error("inet_pton");
+    int res_pton1, res_pton2;
+    res_pton1 = inet_pton(AF_INET, "192.168.0.144", &(serv_ip_adress.s_addr));
+    res_pton2 = inet_pton(AF_INET, "192.168.0.102", &(client_ip_adress.s_addr));
+    
+    if (res_pton1 != 1 || res_pton2 != 1) handle_error("inet_pton");
+    // printf("inet_pton: %d\n", res_pton1);
+
+
 
 
     // memset(&serv_addr, 0, sizeof(serv_addr));
@@ -100,70 +133,96 @@ int main() {
     // serv_addr.sll_addr;
     // strcat(serv_addr.sll_addr, "2cf05d436ffd");  //mac-adress
     // 2c:f0:5d:43:6f:fd
-    serv_addr.sll_addr[0] = 0xfd;
-    serv_addr.sll_addr[1] = 0x6f;
-    serv_addr.sll_addr[2] = 0x43;
-    serv_addr.sll_addr[3] = 0x5d;
-    serv_addr.sll_addr[4] = 0xf0;
-    serv_addr.sll_addr[5] = 0x2c;
+    serv_addr.sll_addr[5] = 0xfd;
+    serv_addr.sll_addr[4] = 0x6f;
+    serv_addr.sll_addr[3] = 0x43;
+    serv_addr.sll_addr[2] = 0x5d;
+    serv_addr.sll_addr[1] = 0xf0;
+    serv_addr.sll_addr[0] = 0x2c;
 
 
 
     // 2c:f0:5d:43:6f:fd - MY SERV_MAC
     // 84:c8:a0:51:54:f0 - MY CLIENT_MAC
 
+    printf("<<<<1>>>>\n");
+
+
     // MAC HEADER
     mac_hdr = (struct macheader *) buffer;
-    mac_hdr->src_mac[0] = 0xf0;
-    mac_hdr->src_mac[1] = 0x54;
-    mac_hdr->src_mac[2] = 0x51;
-    mac_hdr->src_mac[3] = 0xa0;
-    mac_hdr->src_mac[4] = 0xc8;
-    mac_hdr->src_mac[5] = 0x84;
+    mac_hdr->src_mac[5] = 0xf0;
+    mac_hdr->src_mac[4] = 0x54;
+    mac_hdr->src_mac[3] = 0x51;
+    mac_hdr->src_mac[2] = 0xa0;
+    mac_hdr->src_mac[1] = 0xc8;
+    mac_hdr->src_mac[0] = 0x84;
 
 
-    mac_hdr->dest_mac[0] = 0xfd;
-    mac_hdr->dest_mac[1] = 0x6f;
-    mac_hdr->dest_mac[2] = 0x43;
-    mac_hdr->dest_mac[3] = 0x5d;
-    mac_hdr->dest_mac[4] = 0xf0;
-    mac_hdr->dest_mac[5] = 0x2c;
+    mac_hdr->dest_mac[5] = 0xfd;
+    mac_hdr->dest_mac[4] = 0x6f;
+    mac_hdr->dest_mac[3] = 0x43;
+    mac_hdr->dest_mac[2] = 0x5d;
+    mac_hdr->dest_mac[1] = 0xf0;
+    mac_hdr->dest_mac[0] = 0x2c;
 
 
-    mac_hdr->type = ETHERTYPE_IP; // IPv4 - 0x800
+    mac_hdr->type = htons(ETHERTYPE_IP); // IPv4 - 0x800
 
 
+    printf("<<<<2>>>>\n");
 
 
 
     //IP HEADER
-    ip_hdr = (struct iphdr *) buffer;
+    ip_hdr = (struct iphdr *) (buffer + sizeof(struct macheader));
 
     // add zero !!!!
-    int total_lenght = sizeof(struct iphdr) + sizeof(struct udphdr) + strlen(string_data) + 1;
+    unsigned short total_lenght = sizeof(struct iphdr) + sizeof(struct udphdr) + strlen(msg) + 1;
+    printf("total_lenght: %d\n", total_lenght);
+
 
     ip_hdr->version = 4;
     ip_hdr->ihl = 5;
     ip_hdr->tos = 0;
-    ip_hdr->tot_len = htons(total_lenght) ; 
     ip_hdr->id = htons(0);
+    ip_hdr->tot_len = htons(total_lenght) ; 
     ip_hdr->frag_off = htons(0);
-    ip_hdr->ttl = 1;
+    ip_hdr->ttl = 16;
     ip_hdr->protocol = IPPROTO_UDP;
-    ip_hdr->saddr = inet_addr("127.0000.0000.0001");
-    ip_hdr->daddr = inet_addr("127.0000.0000.0001");  // 0x7F000001 - 127.0.0.1
+    ip_hdr->saddr = client_ip_adress.s_addr;
+    ip_hdr->daddr = serv_ip_adress.s_addr;  // 0x7F000001 - 127.0.0.1
     ip_hdr->check = htons(0);
 
+    // printf("<<<<2.5>>>>\n");
 
 
-    int csum = check_sum(buffer);
+    // int csum = check_sum(buffer + sizeof(struct macheader));
+    int csum = 0;
+    short *ptr;
+
+    ptr = (short *) (buffer + sizeof(struct macheader));
+
+    for (int i = 0; i < 10; i++) {
+        csum = csum + *ptr; // maybe + *ptr ??? 
+        ptr++;
+    }
+
+    int tmp = csum >> 16;
+    csum = (csum & 0xFFFF) + tmp;
+    csum = ~csum;
+    
+    
+    
     printf("csum: %d\n", csum);
     ip_hdr->check = csum;   // always filled in
 
 
 
+    printf("<<<<3>>>>\n");
+
+
     // UDP HEADER
-    udp_hdr = (struct updhdr *) (buffer + sizeof(struct iphdr));
+    udp_hdr = (struct updhdr *) (buffer + sizeof(struct iphdr) + sizeof(struct macheader));
 
     udp_hdr->source = htons(CLIENT_PORT);
     udp_hdr->dest = htons(SERV_PORT);
@@ -176,16 +235,19 @@ int main() {
 
 
     /* copy the data after the UDP header */
-    string_data = (char *) (buffer + sizeof(struct iphdr) + sizeof(struct udphdr));
+    string_data = (char *) (buffer + sizeof(struct macheader) + sizeof(struct iphdr) + sizeof(struct udphdr));
     strncpy(string_data, msg, strlen(msg));
 
     // printf("string_data: %s\n", string_data);
 
+    printf("<<<<4>>>>\n");
 
 
-    if (sendto(fd, buffer, total_lenght, 0, (struct sockaddr *) &serv_addr, serv_size) == -1) handle_error("setdto");
+    if (sendto(fd, buffer, total_lenght + sizeof(struct macheader), 0, (struct sockaddr *) &serv_addr, serv_size) == -1) handle_error("setdto");
     sleep(1);
   
+    printf("<<<<5>>>>\n");
+
     while (1) {
         if (recvfrom(fd, recv_buf, (sizeof(struct iphdr) + sizeof(struct udphdr)+strlen(string_data)+1), 0, NULL, NULL) == -1) handle_error("recfrom");
         udp_hdr = (struct udphdr *)(recv_buf + sizeof(struct iphdr));
@@ -204,6 +266,7 @@ int main() {
     close(fd);
     return 0;
 }
+
 
 
 
